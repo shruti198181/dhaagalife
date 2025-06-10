02f5724dfc5459e1e8713c4bde4386cc6bbcaf2a
shruti198181 <shahshruti503@gmail.com>
2025-06-10 11:29:37 +0530

    changes

--- src/component/slider.jsx
+++ src/component/slider.jsx
@@ -47,7 +47,7 @@ function Sliders () {
                 >
                 
                 <Carousel.Item>
-                    <Row>
+                    <Row style={{display:'flex',justifyContent:'sm-start'}}>
                         <Col xs={6} sm={3} className='text-center text-sm-start'>
                          <Image 
                            src={'https://cdn.shopify.com/s/files/1/0605/0558/7893/files/Dhaaga_13042024_5187_400x400.jpg?v=1742630432'}
